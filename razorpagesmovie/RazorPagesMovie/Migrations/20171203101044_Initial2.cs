43a20df07c2d869b2f9388345f37aa70ccc47cff
kimjohnconcha <kimjohnconcha@icloud.com>
2017-12-03 19:33:29 +0800

    Adding a model

---
+++ razorpagesmovie/RazorPagesMovie/Migrations/20171203101044_Initial2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace RazorPagesMovie.Migrations
+{
+    public partial class Initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ razorpagesmovie/RazorPagesMovie/Models/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
